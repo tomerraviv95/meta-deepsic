cf456b44cdfff52a12209c169217aa8dc07410c0
tomer raviv <tomerraviv95@gmail.com>
2022-01-09 21:44:24 +0200

    Updated config for figures

--- python_code/plotting/plot_figures_for_paper.py
+++ python_code/plotting/plot_figures_for_paper.py
@@ -6,32 +6,34 @@ import matplotlib.pyplot as plt
 
 # plot figures for the paper
 def plot_figure_wrapper(figure_ind: int):
-    if figure_ind in [1, 2, 5, 6, 7, 8, 11, 12, 15, 16, 17, 18, 23]:
+    if figure_ind in [1, 4, 7, 10, 15]:
         plotter.ser_versus_block(current_run_params=get_deepsic(figure_ind))
-        if figure_ind in [7, 8, 17, 18, 23]:
+        if figure_ind in [4, 10, 15]:
             plotter.ser_versus_block(current_run_params=get_resnet(figure_ind))
         plotter.ser_versus_block(current_run_params=get_online_deepsic(figure_ind))
-        if figure_ind in [7, 8, 17, 18, 23]:
+        if figure_ind in [4, 10, 15]:
             plotter.ser_versus_block(current_run_params=get_online_resnet(figure_ind))
         plotter.ser_versus_block(current_run_params=get_meta_deepsic(figure_ind))
-        if figure_ind in [7, 8, 17, 18, 23]:
+        if figure_ind in [4, 10, 15]:
             plotter.ser_versus_block(current_run_params=get_meta_resnet(figure_ind))
-    if figure_ind in [3, 4, 13, 14]:
+    if figure_ind in [2, 8]:
         plotter.ser_versus_block(current_run_params=get_online_deepsic(f'{figure_ind}a'))
         plotter.ser_versus_block(current_run_params=get_online_deepsic_single_user(f'{figure_ind}b'))
-    if figure_ind in [5, 6, 15, 16]:
-        plotter.ser_versus_block(current_run_params=get_deepsic(figure_ind))
-        plotter.ser_versus_block(current_run_params=get_online_deepsic_single_user(figure_ind))
-        plotter.ser_versus_block(current_run_params=get_meta_deepsic_single_user(figure_ind))
-    if figure_ind in [9, 19]:
+    if figure_ind in [3, 9]:
+        plotter.ser_versus_block(current_run_params=get_deepsic(f'{figure_ind}a'))
+        plotter.ser_versus_block(current_run_params=get_online_deepsic(f'{figure_ind}a'))
+        plotter.ser_versus_block(current_run_params=get_online_deepsic_single_user(f'{figure_ind}b'))
+        plotter.ser_versus_block(current_run_params=get_meta_deepsic(f'{figure_ind}a'))
+        plotter.ser_versus_block(current_run_params=get_meta_deepsic_single_user(f'{figure_ind}b'))
+    if figure_ind in [5, 11]:
         plotter.ser_versus_blocks_num(current_run_params=get_deepsic(figure_ind))
         plotter.ser_versus_blocks_num(current_run_params=get_online_deepsic(figure_ind))
         plotter.ser_versus_blocks_num(current_run_params=get_meta_deepsic(figure_ind))
-    if figure_ind in [10, 20]:
+    if figure_ind in [6, 12]:
         plotter.ser_versus_blocks_num(current_run_params=get_deepsic(figure_ind))
         plotter.ser_versus_blocks_num(current_run_params=get_online_deepsic_single_user(figure_ind))
         plotter.ser_versus_blocks_num(current_run_params=get_meta_deepsic_single_user(figure_ind))
-    if figure_ind in [21, 22, 24]:
+    if figure_ind in [13, 14, 16]:
         plotter.ser_versus_snr(current_run_params=get_deepsic(figure_ind))
         plotter.ser_versus_snr(current_run_params=get_resnet(figure_ind))
         plotter.ser_versus_snr(current_run_params=get_online_deepsic(figure_ind))
@@ -41,7 +43,7 @@ def plot_figure_wrapper(figure_ind: int):
 
 
 if __name__ == "__main__":
-    plotter = Plotter(run_over=False)
-    figure_ind = 5
+    plotter = Plotter(run_over=True)
+    figure_ind = 15
     plot_figure_wrapper(figure_ind)
     plt.show()
